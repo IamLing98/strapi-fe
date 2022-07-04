0753e3a153d62f6669c4e6bdb321025daa2b302a
Đoàn Văn Linh <42459014+IamLing98@users.noreply.github.com>
2022-07-04 16:48:06 +0700

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 const baseApiUrl =
   process.env.NODE_ENV === "production"
-    ? "http://18.136.181.250"
-    : "http://18.136.181.250";
+    ? "http://18.136.181.250:1337"
+    : "http://18.136.181.250:1337";
 
 export default baseApiUrl;
